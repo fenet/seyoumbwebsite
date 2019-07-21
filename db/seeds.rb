03925b4161ec364fe5b8c6aeec8279b88fbe3270
fenet <fenetgucci@gmail.com>
2019-07-21 15:10:47 +0300

    seed.rb modified

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+AdminUser.create!(email: "#{Rails.application.credentials.production[:email]}",
+                  password: "#{Rails.application.credentials.production[:seed_password]}",
+                  password_confirmation: "#{Rails.application.credentials.production[:seed_password]}")if Rails.env.production?
+                  
